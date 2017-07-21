9644d3e13bfee6909c4c2b51c2591189ec139512
Louis Linehan <louis@greynoise.co.uk>
2017-07-21 11:01:19 +0100

    Completed ArrayDeclarationUnitTest

--- CodeIgniter4/Tests/Arrays/ArrayDeclarationUnitTest.php
+++ CodeIgniter4/Tests/Arrays/ArrayDeclarationUnitTest.php
@@ -10,10 +10,24 @@
 namespace CodeIgniter4\Tests\Arrays;
 
 use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
-use \CodeIgniter4\Util\Common;
 
 class ArrayDeclarationUnitTest extends AbstractSniffUnitTest
 {
+
+
+    /**
+     * Get a list of CLI values to set before the file is tested.
+     *
+     * @param string                  $testFile The name of the file being tested.
+     * @param \PHP_CodeSniffer\Config $config   The config data for the test run.
+     *
+     * @return void
+     */
+    public function setCliValues($testFile, $config)
+    {
+        $config->tabWidth = 4;
+
+    }//end setCliValues()
 
 
     /**
@@ -22,34 +36,36 @@ class ArrayDeclarationUnitTest extends AbstractSniffUnitTest
      * The key of the array should represent the line number and the value
      * should represent the number of errors that should occur on that line.
      *
-     * Note 1.
-     * Cheating a little here, because PHPCS counts tabs as 1 column.
-     * Assuming tabs are 4 spaces. The indented token will start
-     * column 3 for 2 tabs, but if it were indented with spaces it would
-     * start at column 9.
-     * Elsewhere in the standard we fix spaced indentation with tabs and
-     * everything works but I haven't been able to fix it in this unit test.
-     *
      * @return array<int, int>
      */
     public function getErrorList()
     {
         return array(
+                5  => 1,
+                6  => 1,
+                7  => 1,
+                8  => 1,
+                9  => 1,
                 10 => 1,
                 11 => 1,
                 12 => 1,
-                13 => 1,
+                13 => 2,
                 14 => 1,
                 15 => 1,
-                16 => 1,
-                17 => 1,
-                18 => 2,
-                19 => 1,
-                20 => 1,
-                // See Note 1.
-                25 => 1,
-                // See Note 1.
+                21 => 1,
                 28 => 1,
+                29 => 1,
+                35 => 1,
+                36 => 2,
+                41 => 2,
+                42 => 2,
+                43 => 2,
+                44 => 2,
+                46 => 2,
+                47 => 2,
+                48 => 2,
+                51 => 1,
+                55 => 1,
                );
 
     }//end getErrorList()
