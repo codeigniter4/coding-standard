5498ab3880cb8b9bf683a17684a01ee0a5b51598
Louis Linehan <louis@greynoise.co.uk>
2017-07-25 09:54:49 +0100

    Verticle empty lines

---
+++ CodeIgniter4/Tests/WhiteSpace/VerticalEmptyLinesUnitTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Vertical Empty Lines Unit Test
+ *
+ * @package   CodeIgniter4-Standard
+ * @author    Louis Linehan <louis.linehan@gmail.com>
+ * @copyright 2017 Louis Linehan
+ * @license   https://github.com/louisl/CodeIgniter4-Standard/blob/master/LICENSE MIT License
+ */
+
+namespace CodeIgniter4\Tests\WhiteSpace;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+class VerticalEmptyLinesUnitTest extends AbstractSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return array(
+                13 => 1,
+                19 => 1,
+                23 => 1,
+                24 => 1,
+                30 => 1,
+                38 => 1,
+                41 => 1,
+                44 => 1,
+                45 => 1,
+                46 => 1,
+                47 => 1,
+               );
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return array();
+
+    }//end getWarningList()
+
+
+}//end class
