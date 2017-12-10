1ebedbe84e9bbb4e20962c957ac709201d638137
Louis Linehan <louis@greynoise.co.uk>
2017-12-10 12:13:06 +0000

    Boolean and unit tests

---
+++ CodeIgniter4/Tests/Operators/BooleanAndUnitTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Boolean And Unit Test
+ *
+ * @package   CodeIgniter4-Standard
+ * @author    Louis Linehan <louis.linehan@gmail.com>
+ * @copyright 2017 Louis Linehan
+ * @license   https://github.com/louisl/CodeIgniter4-Standard/blob/master/LICENSE MIT License
+ */
+
+namespace CodeIgniter4\Tests\Operators;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+class BooleanAndUnitTest extends AbstractSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return array(
+                3 => 1,
+                4 => 1,
+                5 => 1,
+                7 => 2,
+               );
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return array();
+
+    }//end getWarningList()
+
+
+}//end class
